50963d1ba2c9cfa7f814b6bd66c533f0843cde93
ThomasB-Sunology <t.blanchard@sunology.eu>
2023-12-29 17:19:50 +0100

    feat: add typography docs

--- packages/ui/src/Atoms/Typography/Typography.stories.tsx
+++ packages/ui/src/Atoms/Typography/Typography.stories.tsx
@@ -1,24 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Suiktheme from '@sunology/tailwind-preset'
 import { Typography } from './Typography'
 
 const meta: Meta = {
 	title: 'Guidelines/Typography',
-	render: () => {
-		return (
-			<div className="flex flex-col flex-wrap gap-4">
-				{Suiktheme.theme.fontSize &&
-					Object.keys(Suiktheme.theme.fontSize)
-						.reverse()
-						.map((fs) => {
-							return (
-								<Typography intent={fs} key={fs}>
-									Lorem ipsum.
-								</Typography>
-							)
-						})}
-			</div>
-		)
+	component: Typography,
+	args: {
+		children: 'Lorem ipsum',
 	},
 }
 
